57f45f2207b187fc3aba70a2947e272b8aa2829a
Isaac Phiri <isaacphiri315@gmail.com>
2025-07-23 12:51:00 +0200

    edit: removed unused imports

--- categorizer/main.py
+++ categorizer/main.py
@@ -2,7 +2,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from api.routes import router as api_router
-from api.auth import auth_router
 
 app = FastAPI()
 app.include_router(auth_router, prefix="/auth")     # OAuth endpoints
