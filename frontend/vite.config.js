540ceaac291eeeabe4154e0283f98005896ed244
Isaac Phiri <isaacphiri315@gmail.com>
2025-07-25 08:53:48 +0200

    Add: frontend file

---
+++ frontend/vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+
+  server: {
+    // frontend
+    host: "0.0.0.0",
+    port: process.env.PORT || 3000,
+
+    // backend server
+    proxy: {
+      '/api': {
+        target: 'https://google-drive-organizer.onrender.com',
+      }
+    },
+  },
+})
+
