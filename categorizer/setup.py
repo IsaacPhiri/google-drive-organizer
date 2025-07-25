ca6656000c123fc3c2dd44bb127674a5527490b5
Isaac Phiri <isaacphiri315@gmail.com>
2025-07-25 08:46:30 +0200

    delete: changed folder name to backend

--- categorizer/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="hsf-backend",
-    version="0.1.0",
-    description="Backend for Heritage Square Foundation project",
-    packages=find_packages()
-)
